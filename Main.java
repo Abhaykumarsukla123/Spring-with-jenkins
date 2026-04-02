fa9d90ec54db09d7ac79c39a7074d6e882ec0ffd
Abhay <abhay.shukla@navyuginfo.com>
2026-04-02 17:40:03 +0530

    added new print statement

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ class Main{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
                 System.out.println("Jenkins Project");
+                                System.out.println("Jenkins Project second commit");
+
 
     }
 }
